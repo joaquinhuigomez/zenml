7686872e5d322ad5b4aee32ebe9c3dfa4abf4d22
Iason Andriopoulos <iason.ext@zenml.io>
2026-03-31 10:00:39 +0300

    Extend SDK

--- src/zenml/models/__init__.py
+++ src/zenml/models/__init__.py
@@ -335,6 +335,7 @@ from zenml.models.v2.core.triggers import (
     PlatformEventTriggerResponse,
     PlatformEventTriggerResponseBody,
     PlatformEventTrigger,
+    SourceEntity,
     TRIGGER_UPDATE_TYPE_UNION,
     TRIGGER_CREATE_TYPE_UNION,
     TRIGGER_RETURN_TYPE_UNION,
@@ -901,4 +902,5 @@ __all__ = [
     "TRIGGER_UPDATE_TYPE_UNION",
     "TRIGGER_CREATE_TYPE_UNION",
     "TRIGGER_RETURN_TYPE_UNION",
+    "SourceEntity",
 ]
